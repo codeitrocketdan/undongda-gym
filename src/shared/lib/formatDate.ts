39b7bf27982563b1a5c762cbcbe92fc8e1594da6
YangMinYeol <myyang@gdnps.com>
2026-05-18 21:54:56 +0900

    feat: 날짜 유틸 함수 구현

---
+++ src/shared/lib/formatDate.ts
@@ -0,0 +1,30 @@
+import { format } from "date-fns";
+
+/**
+ * 2026.05.18 형식으로 날짜 포맷팅
+ * @param date
+ * @returns
+ */
+export function formatDate(date: string | null) {
+  if (!date) return "";
+  return format(new Date(date), "yyyy.MM.dd");
+}
+
+/**
+ * 3분전, 5시간 전 등으로 날짜 포맷팅
+ * @param date
+ * @returns
+ */
+export function formatRelativeDate(date: string | null) {
+  if (!date) return "";
+
+  const now = new Date();
+  const target = new Date(date);
+  const diffMs = now.getTime() - target.getTime();
+  const diffMin = Math.floor(diffMs / 1000 / 60);
+  const diffHour = Math.floor(diffMin / 60);
+
+  if (diffMin < 60) return `${diffMin}분 전`;
+  if (diffHour < 24) return `${diffHour}시간 전`;
+  return formatDate(date);
+}
