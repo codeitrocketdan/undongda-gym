3bbb9ad39bf6c286a308123537a13857bc9894b7
YangMinYeol <myyang@gdnps.com>
2026-05-22 09:21:06 +0900

    fix: FeedCardBody 불필요한 border 제거

--- src/shared/ui/FeedCard/FeedCardBody.tsx
+++ src/shared/ui/FeedCard/FeedCardBody.tsx
@@ -8,8 +8,6 @@ interface FeedCardBodyProps {
 
 export default function FeedCardBody({ children, className }: FeedCardBodyProps) {
   return (
-    <div className={twMerge("flex flex-1 flex-col border-slate-200 pt-2 pb-6 md:pt-4", className)}>
-      {children}
-    </div>
+    <div className={twMerge("flex flex-1 flex-col pt-2 pb-6 md:pt-4", className)}>{children}</div>
   );
 }
