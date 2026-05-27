3400371104fc1537ba1134f7a7adb0516dc04409
handy-o <dyhan9420@gmail.com>
2026-05-27 15:54:58 +0900

    fix: useModal 컴파운드 종속 제거

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,16 +1,16 @@
+"use client";
 import { createContext, ReactNode, useContext } from "react";
+import { createPortal } from "react-dom";
 import { useFocusTrap } from "../../lib/useFocusTrap";
 import ModalBackground from "./Background";
 import CloseButton from "./CloseButton";
 import ModalFooter from "./Footer";
 import ModalHeader from "./Header";
-import { useModal } from "./useModal";
 
 interface ModalComponent extends React.FC<ModalProps> {
   Header: typeof ModalHeader;
   Footer: typeof ModalFooter;
   CloseButton: typeof CloseButton;
-  useModal: typeof useModal;
 }
 
 // 1. 모달의 기능을 공유할 컨텍스트 생성
@@ -32,7 +32,7 @@ interface ModalProps {
 const Modal = ({ children, onClose, isClickToClose }: ModalProps) => {
   const trapRef = useFocusTrap<HTMLDivElement>();
 
-  return (
+  return createPortal(
     <ModalContext.Provider value={{ onClose }}>
       <ModalBackground isClickToClose={isClickToClose}>
         <div
@@ -41,19 +41,19 @@ const Modal = ({ children, onClose, isClickToClose }: ModalProps) => {
           onKeyDown={(e) => {
             if (e.key === "Escape") onClose();
           }}
-          className="max-w-140 min-w-85 bg-white p-10"
           onClick={(e) => e.stopPropagation()}
+          className="max-w-140 min-w-85 bg-white p-10"
         >
           {children}
         </div>
       </ModalBackground>
-    </ModalContext.Provider>
+    </ModalContext.Provider>,
+    document.body
   );
 };
 
 (Modal as ModalComponent).Header = ModalHeader;
 (Modal as ModalComponent).Footer = ModalFooter;
 (Modal as ModalComponent).CloseButton = CloseButton;
-(Modal as ModalComponent).useModal = useModal;
 
 export default Modal as ModalComponent;
