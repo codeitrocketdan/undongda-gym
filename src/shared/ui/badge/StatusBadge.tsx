a532d9cca47022a832623e7613b03a6674d65d57
YangMinYeol <myyang@gdnps.com>
2026-05-29 14:14:31 +0900

    fix: 뱃지파일 모듈화 및 디테일 변경

--- src/shared/ui/badge/StatusBadge.tsx
+++ src/shared/ui/badge/StatusBadge.tsx
@@ -1,7 +1,7 @@
-import { Check } from "lucide-react";
+import { MeetingStatus } from "@/shared/lib/getMeetingStatus";
 import { twMerge } from "tailwind-merge";
 
-type Status = "upcoming" | "completed" | "pending" | "confirmed";
+type Status = MeetingStatus;
 
 interface StatusBadgeProps {
   status: Status;
@@ -12,34 +12,15 @@ const statusConfig: Record<Status, { label: string; className: string }> = {
     label: "이용예정",
     className: "bg-blue-200 text-blue-600",
   },
-  pending: {
-    label: "개설대기",
-    className: "bg-slate-100 text-slate-600",
-  },
   completed: {
     label: "이용완료",
     className: "bg-slate-100 text-slate-600",
-  },
-  confirmed: {
-    label: "개설확정",
-    className: "",
   },
 };
 
 export default function StatusBadge({ status }: StatusBadgeProps) {
   const { label, className } = statusConfig[status];
 
-  if (status === "confirmed") {
-    return (
-      <div className="flex min-w-max items-center justify-center gap-0.5 rounded-3xl border border-blue-500 px-3 py-1.5">
-        <Check
-          size={18}
-          className="bg-gradient-blue-500 rounded-full text-white"
-        />
-        <span className="text-sm text-blue-600">{label}</span>
-      </div>
-    );
-  }
   return (
     <div
       className={twMerge(
--- src/shared/ui/status-label/StatusLabel.tsx
+++ src/shared/ui/status-label/StatusLabel.tsx
@@ -5,7 +5,7 @@ export default function StatusLabel() {
     <div className="flex min-w-max items-center justify-center gap-0.5">
       <Check
         size={14}
-        className="bg-gradient-blue-500 rounded-full text-white md:size-4.5"
+        className="bg-gradient-blue-500 rounded-full text-white md:size-4"
       />
       <span className="text-xs text-blue-600 md:text-sm">개설확정</span>
     </div>
