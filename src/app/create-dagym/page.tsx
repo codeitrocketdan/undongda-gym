c4aa2356f316974a2753bfd4f9e8e844624ae865
handy-o <dyhan9420@gmail.com>
2026-06-05 11:48:49 +0900

    fix: 미사용 파일 제거

--- src/app/create-dagym/page.tsx
+++
@@ -1,5 +0,0 @@
-import CreateDagymPage from "@/page/create-dagym/page";
-
-export default function Page() {
-  return <CreateDagymPage />;
-}
--- src/page/create-dagym/page.tsx
+++
@@ -1,5 +0,0 @@
-import { CreateDagymForm } from "@/features/create-dagym";
-
-export default function Page() {
-  return <CreateDagymForm />;
-}
