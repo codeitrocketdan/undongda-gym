d634d499a84229aa3b1b2484c2705ccd47fdfe8f
handy-o <dyhan9420@gmail.com>
2026-06-11 11:33:43 +0900

    feat: test 코드 추가

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,20 @@ const config: Config = {
   testEnvironment: "jsdom",
   // 테스트 전에 실행할 설정 파일을 지정
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  // Swiper 등 CSS 파일 임포트 에러 해결을 위한 매퍼 설정
+  moduleNameMapper: {
+    // CSS 파일을 만나면 에러를 내지 말고, 빈 객체({})를 내뱉는 모듈로 대체해라!
+    "^swiper/css$": "<rootDir>/__mocks__/styleMock.ts",
+    "\\.(css|less|sass|scss)$": "<rootDir>/__mocks__/styleMock.ts",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config);
+export default async function jestConfig() {
+  const makeConfig = createJestConfig(config);
+  const finalConfig = await makeConfig();
+
+  finalConfig.transformIgnorePatterns = [];
+
+  return finalConfig;
+}
