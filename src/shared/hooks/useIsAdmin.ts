cac5bf440e347f02a2a66a6aff5f01935d9af40a
handy-o <dyhan9420@gmail.com>
2026-06-22 01:31:54 +0900

    feat: 관리자 계정 드롭다운에 관리자 페이지 이동 버튼 추가

--- src/shared/hooks/useIsAdmin.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useUser } from "@/shared/hooks/useUser";
-
-const ADMIN_EMAIL = "admin@admin.com";
-
-export function useIsAdmin() {
-  const { user } = useUser();
-  return user?.email === ADMIN_EMAIL;
-}
--- src/shared/ui/header/Header.tsx
+++ src/shared/ui/header/Header.tsx
@@ -21,7 +21,7 @@ export default async function Header() {
   const isLogin = !!cookieStore.get("accessToken")?.value;
 
   return (
-    <header>
+    <header className="fixed top-0 left-0 z-100 w-full bg-[var(--color-background)]">
       <div className="relative mx-auto mb-4 flex w-full max-w-7xl items-center justify-between p-6">
         <div className="flex items-center gap-4">
           {isLogin && (
