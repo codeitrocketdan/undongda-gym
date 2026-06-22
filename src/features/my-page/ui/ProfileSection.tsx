ba680815f6884969c7455ad1b6e77fd13bc25a53
YangMinYeol <sheep0125@naver.com>
2026-06-22 10:10:45 +0900

    fix: 마이페이지 이미지 비율 조정

--- src/features/my-page/ui/ProfileSection.tsx
+++ src/features/my-page/ui/ProfileSection.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { useUser } from "@/shared/hooks/useUser";
 import avatar from "@/shared/assets/images/avatar.svg";
-import Skeleton from "@/shared/ui/skeleton/Skeleton";
+import { useUser } from "@/shared/hooks/useUser";
+import { useLogout } from "@/shared/ui/header/useLogout";
 import { useModal } from "@/shared/ui/modal";
+import Skeleton from "@/shared/ui/skeleton/Skeleton";
 import { Pencil } from "lucide-react";
 import Image from "next/image";
 import ProfileEditModal from "./ProfileEditModal";
-import { useLogout } from "@/shared/ui/header/useLogout";
 
 export default function ProfileSection() {
   const logout = useLogout();
@@ -35,16 +35,16 @@ export default function ProfileSection() {
 
   return (
     <div className="gradient-blue-light relative flex flex-row gap-6 rounded-2xl border border-blue-300 p-4 md:rounded-3xl md:p-6 lg:flex-col lg:items-center lg:justify-center lg:gap-4 lg:px-10 lg:py-8">
-      <div className="relative flex h-20 w-20 items-center justify-center rounded-full bg-white md:h-28 md:w-28">
+      <div className="relative flex h-20 w-20 items-center justify-center overflow-hidden rounded-full bg-white md:h-28 md:w-28">
         {profile.image ? (
           <Image
             src={profile.image}
             alt={profile.name}
             fill
-            className="rounded-full"
+            className="object-cover"
           />
         ) : (
-          <Image src={avatar} alt="기본 이미지" fill className="rounded-full" />
+          <Image src={avatar} alt="기본 이미지" fill className="object-cover" />
         )}
       </div>
       <div className="flex flex-col justify-center lg:items-center">
