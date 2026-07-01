be2839f4316561c022412da95cc3b1a376945b8a
Daesung <klp2797@naver.com>
2026-07-01 23:29:28 +0900

    refactor: biome migration

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,5 @@
-import { relative } from "path";
-
-const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames.map((f) => `"${relative(process.cwd(), f)}"`).join(" ")}`;
-
 const config = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx,json}": ["biome check --write"],
 };
 
 export default config;
